41ec5078769771fcfd721c15c0d6d8739a7eff2b
Alex Kalderimis <alex.kalderimis@gmail.com>
2018-12-02 23:40:54 +0000

    Added solution for 02 DEC 2017

---
+++ 2017/02/Main.hs
@@ -0,0 +1,28 @@
+import Data.Foldable
+import Data.Maybe
+import Control.Applicative
+import Data.List
+
+checkRow :: [Int] -> Int
+checkRow xs = let (small, big) = minmax xs
+               in fromMaybe 0 ((-) <$> big <*> small)
+  where
+    minmax = foldl' mm (Nothing, Nothing)
+    mm (sm, bg) x = ((min x <$> sm) <|> Just x, (max x <$> bg) <|> Just x)
+
+pairs :: [a] -> [(a, a)]
+pairs l = [(x,y) | (x:ys) <- tails l, y <- ys]
+
+divisable a b = case (mod a b, mod b a) of
+  (0, _) -> Just (a `div` b)
+  (_, 0) -> Just (b `div` a)
+  _      -> Nothing
+
+divRow :: [Int] -> Int
+divRow = fromMaybe 0 . listToMaybe . catMaybes . fmap (uncurry divisable) . pairs
+
+main :: IO ()
+main = do
+  rows <- fmap (fmap read . words) . lines <$> getContents
+  putStrLn $ "MINMAX: " ++ (show . sum $ fmap checkRow rows)
+  putStrLn $ "DIVISIBLE: " ++ (show . sum $ fmap divRow rows)
