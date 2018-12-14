9f12438e4a1c23033a11f5a7ab602e5e3974fbf2
Alex Kalderimis <alex.kalderimis@gmail.com>
2018-12-14 08:15:52 +0000

    Refactoring for clarity and performance
    This is byte orientated, so makes sense to use real byte orientated
    types (ByteString and Word8). Also this is the classic case for
    using 'interact'.

--- 2017/10/Main.hs
+++ 2017/10/Main.hs
@@ -1,7 +1,11 @@
-{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE DeriveFunctor     #-}
+{-# LANGUAGE OverloadedStrings #-}
 
-import           Data.Bits          (shiftR, xor)
+import qualified Data.Array         as A
+import           Data.Bits          (Bits, shiftR, xor)
+import qualified Data.ByteString    as B
 import           Data.Foldable      hiding (toList)
+import           Data.Word
 import           System.Environment
 import           System.Exit
 import           Test.Hspec
@@ -17,49 +21,56 @@ main :: IO ()
 main = do
   args <- getArgs
   case args of
-    ["pt1"] -> pt1
-    ["pt2"] -> pt2
-    _       -> die "bad arguments, expected pt1, or pt2"
+    ["pt1"]  -> pt1
+    ["pt2"]  -> pt2
+    ["test"] -> withArgs [] (hspec spec)
+    _        -> die "bad arguments, expected pt1, or pt2"
 
 pt1 :: IO ()
 pt1 = do
   -- this is possibly the dumbest possible way to parse input
+  -- only acceptable because we know exactly what the input is.
   lens <- read . ('[':) . (++ "]") <$> getContents
   print (pinchHash 256 lens)
 
 pt2 :: IO ()
-pt2 = getContents >>= putStrLn . asciiHash . concat . lines
+pt2 = B.interact hash >> putStrLn ""
 
-asciiHash :: String -> String
-asciiHash input =
-  let salt = [17, 31, 73, 47, 23]
-      bytes = fmap ((`mod` 256) . fromEnum) input ++ salt
-      hash z (skip, len) = pinch len skip z
-      n = length bytes
-   in hexadecimal . dense
-                 . toList . rewind
-                 . foldl' hash (fromList [0 .. 255])
-                 . zip [0 ..]
-                 $ take (n * 64) (cycle bytes)
+hash :: B.ByteString -> B.ByteString
+hash input =
+  let salt  = [17, 31, 73, 47, 23]
+      bytes = B.unpack input ++ salt
+      n     = B.length input + 5
+      p z (skip, len) = pinch 256 len skip z
+   in B.pack . hexadecimal . dense
+             . toList . rewind
+             . foldl' p (fromList [0 .. 255])
+             . zip [0 ..]
+             . take (n * 64)
+             . cycle
+             $ fmap fromIntegral bytes
 
 pinchHash :: Int -> [Int] -> Int
 pinchHash n
   = product . take 2 . toList . rewind . foldl' pinch' z0 . zip [0 ..]
   where
     z0 = fromList (take n [0 ..])
-    pinch' z (skip, len) = pinch len skip z
+    pinch' z (skip, len) = pinch n len skip z
 
-dense :: [Int] -> [Int]
+dense :: Bits byte => [byte] -> [byte]
 dense [] = []
 dense xs = let (as,bs) = splitAt 16 xs
                 in foldl1 xor as : dense bs
 
-hexadecimal :: [Int] -> String
+hexChars :: A.Array Word8 Word8
+hexChars = A.listArray (0,15)
+  $ fmap (toEnum . fromEnum) (['0' .. '9'] ++ ['a' .. 'f'])
+
+hexadecimal :: [Word8] -> [Word8]
 hexadecimal = (>>= byteToHex)
   where byteToHex b = let h = b `shiftR` 4
                           l = b `mod` 16
-                       in [chars !! h, chars !! l]
-        chars = ['0' .. '9'] ++ ['a' .. 'f']
+                       in [hexChars A.! h, hexChars A.! l]
 
 fromList :: [a] -> Zipper a
 fromList []     = error "cannot build zipper for empty list"
@@ -81,28 +92,34 @@ shift n z
   | n < 0     = shift (succ n) (left z)
   | otherwise = shift (pred n) (right z)
 
+shiftTo :: Int -> Zipper a -> Zipper a
+shiftTo i z = shift (i - idx z) z
+
 toList :: Zipper a -> [a]
 toList z = focus z : rights z ++ reverse (lefts z)
 
-pinch :: Int -> Int -> Zipper a -> Zipper a
-pinch len skip (Zipper i ls a rs)
-  = let xs = a : rs ++ reverse ls
-        (pinched, rst) = splitAt len xs
-        z = (fromList (reverse pinched ++ rst)) { idx = i }
-    in shift ((len + skip) `mod` length xs) z
+-- perform a round of the pinch hash.
+-- This takes the length of the zipper to avoid having to recalculate it
+-- each round.
+pinch :: Int -> Int -> Int -> Zipper a -> Zipper a
+pinch zlen len skip z
+  = let (pinched, rst) = splitAt len (toList z)
+        z'             = (fromList (reverse pinched ++ rst)) { idx = idx z }
+        idx'           = (idx z + len + skip) `mod` zlen
+    in shiftTo idx' z'
 
 rewind :: Zipper a -> Zipper a
-rewind z = shift (negate $ idx z) z
+rewind = shiftTo 0
 
 spec :: Spec
 spec = do
   describe "asciiHash" $ do
     it "can hash the empty string" $ do
-      asciiHash "" `shouldBe` "a2582a3a0e66e6e86e3812dcb672a272"
+      hash "" `shouldBe` "a2582a3a0e66e6e86e3812dcb672a272"
     it "can hash AoC 2017" $ do
-      asciiHash "AoC 2017" `shouldBe` "33efeb34ea91902bb2f59c9920caa6cd"
+      hash "AoC 2017" `shouldBe` "33efeb34ea91902bb2f59c9920caa6cd"
     it "can hash 1,2,3" $ do
-      asciiHash "1,2,3" `shouldBe` "3efbe78a8d82f29979031a4aa0b16a9d"
+      hash "1,2,3" `shouldBe` "3efbe78a8d82f29979031a4aa0b16a9d"
     it "can hash 1,2,4" $ do
-      asciiHash "1,2,4" `shouldBe` "63960835bcdc130f0b66d7ff4f6a5a8e"
+      hash "1,2,4" `shouldBe` "63960835bcdc130f0b66d7ff4f6a5a8e"
 
