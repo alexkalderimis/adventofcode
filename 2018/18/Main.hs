16c7d1d5901cc963055d7b5839ae3d3b52982d6e
Alex Kalderimis <alex.kalderimis@gmail.com>
2018-12-19 10:49:01 +0000

    Passing pt1 and pt2
    Another game-of-life-alike. Again with loops for efficiency.

--- 2018/18/Main.hs
+++ 2018/18/Main.hs
@@ -1,19 +1,22 @@
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE LambdaCase        #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TupleSections     #-}
 
 import           Control.Applicative
-import qualified Data.Array              as A
+import           Control.Monad
+import qualified Data.Array          as A
+import           Data.Foldable       (foldl')
+import           Data.List           (unfoldr)
+import qualified Data.Map.Strict     as M
 import           Data.Maybe
-import           Data.Foldable           (foldl')
-import           Data.List               (unfoldr)
-import qualified Data.Map.Strict         as M
-import qualified Data.Set                as S
-import           Data.Text               (Text)
-import qualified Data.Text               as Text
-import qualified Data.Text.IO            as Text
+import qualified Data.Set            as S
 import           System.Environment
 import           System.Exit
 import           Text.Printf
+import           Text.Read
+
+import           Options.Generic
 
 data Item = OpenGround | Trees | Lumberyard deriving (Show, Eq, Ord)
 type Coord = (Int,Int)
@@ -33,9 +36,47 @@ exampleInput = unlines
   ,"...#.|..|."
   ]
 
-run n = do
-  let land = parseInput exampleInput
-  mapM_ (putStrLn . showLand) (take n (iterate evolve land))
+data Options = Options
+  { optN     :: Maybe String
+  , optDebug :: Bool
+  } deriving (Generic, Show)
+
+instance ParseRecord Options
+
+main :: IO ()
+main = do
+  opts <- getRecord "AOC Dec 18"
+  n <- case optN opts of
+        Just "pt1" -> pure 10
+        Just "pt2" -> pure 1000000000
+        Just  s    -> maybe (die $ "Bad argument : " ++ s) pure (readMaybe s)
+  getContents >>= run (optDebug opts) n
+
+run :: Bool -> Int -> String -> IO ()
+run debug n input = do
+  let land = looping evolve n (parseInput input)
+  putStrLn (showLand land)
+  printf "Resource value: %d\n" (resourceValue land)
+
+looping :: Ord a => (a -> a) -> Int -> a -> a
+looping f n x = go (M.singleton x n) n x
+  where
+    go _ 0 x = x
+    go m n x =
+      let x'   = f x
+          loop = maybe 0 (loopSize n) (M.lookup x' m)
+          n'   = n - (1 + loop)
+      in go (M.insert x' n' m) n' x'
+    loopSize n loopStart = let loopLen = loopStart - (n - 1)
+                               loops = (n - 1) `div` loopLen
+                            in loops * loopLen
+
+resourceValue :: LandScape -> Int
+resourceValue = uncurry (*) . foldl' translate (0,0) . fmap f . A.elems
+  where
+    f Trees      = (1,0)
+    f Lumberyard = (0,1)
+    f _          = (0,0)
 
 showLand :: LandScape -> String
 showLand land = unlines [row y | y <- [miny .. maxy]]
@@ -43,7 +84,7 @@ showLand land = unlines [row y | y <- [miny .. maxy]]
     ((miny,minx),(maxy,maxx)) = A.bounds land
     row y = [cell (y,x) | x <- [minx .. maxx]]
     cell pos = case land A.! pos of OpenGround -> '.'
-                                    Trees -> '|'
+                                    Trees      -> '|'
                                     Lumberyard -> '#'
 
 parseInput :: String -> LandScape
@@ -55,9 +96,9 @@ parseInput str =
       bounds = ((0,0), (length strs - 1, maximum (fmap length strs) - 1))
    in A.array bounds cells
   where
-    cell '.' = OpenGround
-    cell '|' = Trees
-    cell '#' = Lumberyard
+    cell '.'  = OpenGround
+    cell '|'  = Trees
+    cell '#'  = Lumberyard
     cell char = error $ "Unexpected character: " ++ [char]
 
 evolve :: LandScape -> LandScape
@@ -70,7 +111,7 @@ tick items (pos, x) = (pos, tickWith (count neighbours) x)
                               , let p = shift pos
                               , A.inRange (A.bounds items) p
                               ]
-                    
+
 
 tickWith :: M.Map Item Int -> Item -> Item
 tickWith m OpenGround | num Trees m >= 3 = Trees
