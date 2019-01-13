d6d1e196909eb0e6f0aa98704dfe7f22c59d8a4d
Alex Kalderimis <alex.kalderimis@gmail.com>
2019-01-13 22:52:14 +0000

    Minor changes, more tests

--- src/Elves/Cartesian.hs
+++ src/Elves/Cartesian.hs
@@ -5,6 +5,15 @@ type Location = (Int, Int)
 translate :: Location -> Location -> Location
 translate (dy,dx) (y,x) = (y + dy, x + dx)
 
+adjacent4 :: Location -> [Location]
+adjacent4 pos = fmap ($ pos) [up,down,left,right]
+
+adjacent8 :: Location -> [Location]
+adjacent8 pos = fmap ($ pos) [up,down,left,right
+                             ,up . right, up . left
+                             ,down . right, down . left
+                             ]
+
 up, down, left, right :: Location -> Location
 up    = translate (-1, 0)
 down  = translate ( 1, 0)
--- test/Spec.hs
+++ test/Spec.hs
@@ -3,6 +3,7 @@ import Test.Hspec
 import qualified ElvesSpec as ES
 import qualified Elves.CliqueSpec as C
 import qualified Elves.RTreeSpec as R
+import qualified Elves.KnotSpec as K
 
 main :: IO ()
-main = hspec $ foldr1 (>>) [ ES.spec, C.spec, R.spec ]
+main = hspec $ foldr1 (>>) [ ES.spec, C.spec, R.spec, K.spec ]
