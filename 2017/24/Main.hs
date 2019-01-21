fd73f2a4442c07b701ba0b1c2e00d2998c3732ed
Alex Kalderimis <alex.kalderimis@gmail.com>
2019-01-21 08:49:35 +0000

    Massive performance improvement
    Through the magic of dynamic programming, brings time down from 10sec to
    1.5sec.
    
    * Better search using a tree
    * Remove pointless sorting of child nodes

--- 2017/24/Main.hs
+++ 2017/24/Main.hs
@@ -7,6 +7,9 @@ import           Data.Maybe
 import           Data.Ord
 import qualified Data.Text               as Text
 import           System.Exit
+import           Data.Monoid
+import           Data.Tree (Forest, Tree(..))
+import qualified Data.Tree as Tree
 
 import           Data.Attoparsec.Text    (char, decimal)
 import           Text.Parser.Char        (newline)
@@ -17,6 +20,7 @@ import           Elves.Advent
 
 type Port = Word
 data Component = Component { portA :: Port, portB :: Port } deriving (Show, Eq)
+type Chain = [Component]
 
 instance Ord Component where
   compare a b = compare (strength a, portA a, portB a) (strength b, portA b, portB b)
@@ -24,29 +28,37 @@ instance Ord Component where
 main :: IO ()
 main = day 24 parser pt1 pt2 test
   where
-    pt1 cs = case bestChain chainStrength (chains 0 cs) of
-               Nothing    -> die "No chain found"
-               Just chain -> print (chainStrength $ chain)
-    pt2 cs = case bestChain (length <#> chainStrength) (chains 0 cs) of
-               Nothing    -> die "No chain found"
-               Just chain -> print (chainStrength $ chain)
+    pt1 cs = print . highestStrength $ bridges 0 cs
+    pt2 cs = print . strongestLongest $ bridges 0 cs
 
 test = do
   let mcomps = parseOnly parser exampleComponents
   describe "example" $ do
-    it "pt1" $ do
-      fmap (bestChain chainStrength. chains 0) mcomps
+    it "pt1-find chain" $ do
+      fmap (best chainStrength. chains 0) mcomps
            `shouldBe` Right (Just [Component 0 1
                                   ,Component 1 10
                                   ,Component 9 10
                                   ])
-    it "pt2" $ do
-      fmap (bestChain (length <#> chainStrength) . chains 0) mcomps
+    it "pt1-find strength" $ do
+      fmap (fmap chainStrength . best chainStrength. chains 0) mcomps
+           `shouldBe` Right (Just 31)
+    it "pt1-find strength from bridges" $ do
+      fmap (highestStrength . bridges 0) mcomps
+           `shouldBe` Right 31
+    it "pt2-find chain" $ do
+      fmap (best (length <#> chainStrength) . chains 0) mcomps
            `shouldBe` Right (Just [Component 0 2
                                   ,Component 2 2
                                   ,Component 2 3
                                   ,Component 3 5
                                   ])
+    it "pt2-find strength" $ do
+      fmap (fmap chainStrength . best (length <#> chainStrength) . chains 0) mcomps
+           `shouldBe` Right (Just 19)
+    it "pt2-find strength from bridges" $ do
+      fmap (strongestLongest . bridges 0) mcomps
+           `shouldBe` Right 19
 
   describe "parser" $ do
     it "parses the example correctly" $ do
@@ -60,20 +72,34 @@ test = do
                               ,Component 9 10
                               ]
 
-bestChain :: Ord b => ([Component] -> b) -> [[Component]] -> Maybe [Component]
-bestChain f = listToMaybe . L.sortBy (comparing (Down . f))
+best :: Ord b => (a -> b) -> [a] -> Maybe a
+best f = listToMaybe . L.sortBy (comparing (Down . f))
+
+bestBridges :: (Monoid b, Ord b) => (Component -> b) -> Bridges -> b
+bestBridges f bs = maximum $ mempty : [f (rootLabel t) <> bestBridges f (subForest t) | t <- bs]
+
+highestStrength :: Bridges -> Word
+highestStrength = getSum . bestBridges (Sum . strength)
+
+strongestLongest :: Bridges -> Word
+strongestLongest = getSum . snd . bestBridges ((Sum . const 1) <#> (Sum . strength))
 
 chainStrength :: [Component] -> Word
 chainStrength = sum . fmap strength
 
 -- generates all possible chains going forward from the current port.
-chains :: Port -> [Component] -> [[Component]]
-chains p comps = do
+chains :: Port -> [Component] -> [Chain]
+chains p comps = fromBridges (bridges p comps)
+  where
+    fromBridges bs = bs >>= \t -> fmap (rootLabel t :) ([] : fromBridges (subForest t))
+
+type Bridges = Forest Component
+
+bridges :: Port -> [Component] -> Bridges
+bridges p comps = do
   let cs = zip comps [0 ..]
-  (next,p') <- [(c, port) | c <- L.sortBy (comparing Down) cs
-                          , Just port <- [connectsTo p (fst c)]
-               ]
-  fmap (fst next :) ([] : chains p' [c | (c,x) <- cs, x /= snd next])
+  (next,p') <- [ (c, port) | c <- cs , Just port <- [connectsTo p (fst c)] ]
+  return $ Node (fst next) (bridges p' [c | (c,x) <- cs, x /= snd next])
 
 strength :: Component -> Word
 strength (Component a b) = a + b
