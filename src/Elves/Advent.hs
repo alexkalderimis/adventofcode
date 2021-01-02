b50bb976736882f4d0df8afb6db3b8edf52b8ca7
Alex Kalderimis <akalderimis@gitlab.com>
2021-01-02 17:35:44 +0000

    Comments and timings

--- src/Elves/Advent.hs
+++ src/Elves/Advent.hs
@@ -13,32 +13,52 @@ import           Test.Hspec
 
 type Part a = a -> IO ()
 
--- for exercises with simple input that can be inlined, and this is known statically
-staticDay :: Int -> IO () -> IO () -> Spec -> IO ()
-staticDay n pt1 pt2 spec = do
-  args <- getArgs
-  case args of
-    ["pt1"]  -> pt1
-    ["pt2"]  -> pt2
-    ("test":test_args) -> withArgs test_args
-                          $ hspec (describe ("Day " <> show n) spec)
-    _        -> die "bad arguments. Expected pt1,pt2 or test"
-
-day :: Int -> Parser a -> Part a -> Part a -> Spec -> IO ()
+-- | Combinator to make exercises simpler to write by encapsulating common patterns.
+--
+-- When starting out the following is sufficient:
+--
+-- @
+--   main = day 1 parser pt1 pt2 test
+--     where
+--       parser = pure ()
+--       pt1 = print
+--       pt2 = print
+--       test = pure ()
+-- @
+--
+-- And then each element may be elaborated iteratively.
+--
+day :: Int      -- ^ The day (1..25)
+    -> Parser a -- ^ A Parser for the input, received on stdin
+    -> Part a   -- ^ An implementation for part-one
+    -> Part a   -- ^ An implementation for part-two (available once part-one is solved)
+    -> Spec     -- ^ A test suite
+    -> IO ()    -- ^ The main action
 day n parser pt1 pt2 spec = do
   staticDay n (getInput >>= pt1) (getInput >>= pt2) spec
   where
     getInput = Text.getContents >>= either (die . ("Could not parse input! " ++)) pure
                                     . parseOnly (parser <* (many newline >> endOfInput))
 
+-- for exercises with simple input that can be inlined, and this is known statically
+staticDay :: Int -> IO () -> IO () -> Spec -> IO ()
+staticDay n pt1 pt2 spec = do
+  args <- getArgs
+  case args of
+    ("pt1":rst)  -> withArgs rst (namedTime "pt1" pt1)
+    ("pt2":rst)  -> withArgs rst (namedTime "pt2" pt2)
+    ("test":test_args) -> withArgs test_args
+                          $ hspec (describe ("Day " <> show n) spec)
+    _        -> die "bad arguments. Expected pt1, pt2 or test"
+
 namedTime :: String -> IO () -> IO ()
 namedTime name act = do
   unless (null name) $ putStrLn name >> putStrLn (replicate (length name) '-')
   start <- Clock.getCurrentTime
   act
+  unless (null name) $ putStrLn (replicate (length name) '-')
   end <- Clock.getCurrentTime
   print (Clock.diffUTCTime end start)
-  unless (null name) $ putStrLn (replicate (length name) '-')
 
 time :: IO () -> IO ()
 time = namedTime ""
