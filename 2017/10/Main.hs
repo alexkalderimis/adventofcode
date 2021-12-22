23bb9d0fb390707c757ab6450543c2daccadef5d
Alex Kalderimis <akalderimis@gitlab.com>
2021-12-22 22:49:39 +0000

    Update zipper usage

--- 2017/10/Main.hs
+++ 2017/10/Main.hs
@@ -1,15 +1,14 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import qualified Data.ByteString    as B
+import qualified Data.ByteString    as BS
 import           Data.Foldable      hiding (toList)
 import           Data.Word
 import           System.Environment
 import           System.Exit
 import           Test.Hspec
 
-import qualified Elves.Zipper as Z
+import qualified Elves.CircularBuffer as B
 import qualified Elves.Knot as Knot
-import           Elves.Zipper (Zipper)
 
 main :: IO ()
 main = do
@@ -17,7 +16,7 @@ main = do
   case args of
     ["pt1"]  -> pt1
     ["pt2"]  -> pt2
-    ["test"] -> withArgs [] (hspec spec)
+    ["test"] -> withArgs [] (hspec test)
     _        -> die "bad arguments, expected pt1, or pt2"
 
 pt1 :: IO ()
@@ -28,18 +27,18 @@ pt1 = do
   print (pinchHash 256 lens)
 
 pt2 :: IO ()
-pt2 = B.interact Knot.hash >> putStrLn ""
+pt2 = BS.interact Knot.hash >> putStrLn ""
 
 pinchHash :: Int -> [Int] -> Int
 pinchHash 0 = error "Knot must not be empty"
 pinchHash n
-  = product . take 2 . Z.toList . Z.rewind . foldl' pinch' z0 . zip [0 ..]
+  = product . take 2 . B.toList . B.rewind . foldl' pinch' z0 . zip [0 ..]
   where
-    z0 = foldr Z.insertR (Z.singleton 0) (take (n - 1) [1 ..])
+    z0 = foldr B.insertR (B.singleton 0) (take (n - 1) [1 ..])
     pinch' z (skip, len) = Knot.pinch len skip z
 
-spec :: Spec
-spec = do
+test :: Spec
+test = do
   describe "asciiHash" $ do
     it "can hash the empty string" $ do
       Knot.hash "" `shouldBe` "a2582a3a0e66e6e86e3812dcb672a272"
